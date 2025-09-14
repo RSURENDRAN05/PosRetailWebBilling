c2cd94f81aa953d48c5476e403e769bba24778e2
RSURENDRAN05 <134830197+RSURENDRAN05@users.noreply.github.com>
2025-09-14 13:12:21 +0800

    Ver1.0.0.1
    need to fix

--- ClsModule/functionModule.vb
+++ ClsModule/functionModule.vb
@@ -27,7 +27,7 @@ Module functionModule
     Public _PRINTDS, _SETTINGS As New DataSet
     Public _posPrintHeadDesign As DataSet
     Public Structure M_Details
-        Public Shared SoftwareVersion As String = "Ser-VER25.0.0.19 R2 110925" '"Web" '"Ser" '"Cli"
+        Public Shared SoftwareVersion As String = "Cli-VER25.0.0.21 R2 140925" '"Web" '"Ser" '"Cli"
         Public Shared AppPathDirectory As String = AppDomain.CurrentDomain.BaseDirectory
         Public Shared _appPath As String = Application.StartupPath
         Public Shared LinkAjaxRequest As String = "" ' Initialize empty, set later
