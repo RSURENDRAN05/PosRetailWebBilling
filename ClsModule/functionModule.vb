d4922c243cb4a2db2c2a24c619dec109bdc8f922
RSURENDRAN05 <134830197+RSURENDRAN05@users.noreply.github.com>
2025-10-09 20:45:23 +0800

    tt
    no updated

--- ClsModule/functionModule.vb
+++ ClsModule/functionModule.vb
@@ -28,7 +28,7 @@ Module functionModule
     Public _posPrintHeadDesign As DataSet
 
     Public Structure M_Details
-        Public Shared SoftwareVersion As String = "Cli-VER25.0.0.30 R5 061025" '"Web" '"Ser" '"Cli"
+        Public Shared SoftwareVersion As String = "Cli-VER25.0.0.31 R5 091025" '"Web" '"Ser" '"Cli"
         Public Shared AppPathDirectory As String = AppDomain.CurrentDomain.BaseDirectory
         Public Shared _appPath As String = Application.StartupPath
         Public Shared LinkAjaxRequest As String = "" ' Initialize empty, set later
