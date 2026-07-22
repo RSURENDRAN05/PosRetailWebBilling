2cf9e2d2375a154a09401e93bbd1e2fd77f999c7
RSURENDRAN05 <134830197+RSURENDRAN05@users.noreply.github.com>
2026-07-23 01:29:46 +0800

    Version only Chagned
    Build R19 Server,Pos,Web

--- ClsModule/functionModule.vb
+++ ClsModule/functionModule.vb
@@ -47,7 +47,7 @@ Module functionModule
     End Function
 
     Public Structure M_Details
-        Public Shared SoftwareVersion As String = "Web-VER26.0.0.14 R18 180726 1PM" '"Web" '"Ser" '"Cli"
+        Public Shared SoftwareVersion As String = "Cli-VER26.0.0.17 R19 230726 1AM" '"Web" '"Ser" '"Cli"
         Public Shared AppPathDirectory As String = AppDomain.CurrentDomain.BaseDirectory
         Public Shared _appPath As String = Application.StartupPath
         Public Shared LinkAjaxRequest As String = "" ' Initialize empty, set later
